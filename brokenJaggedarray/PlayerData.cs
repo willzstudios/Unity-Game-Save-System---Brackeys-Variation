1ff65570da1a85513689336530d6a5e45ab9ea3d
willzstudios <83521347+willzstudios@users.noreply.github.com>
2021-06-06 12:52:56 +1000

    Add files via upload
    this is fixed now, the only issue is System.Runtime.Serialization.Formatters.Binary.BinaryFormatter can not serialize Unity Vector2 Jagged Arrays - actually I think it can't even do Vector 2 Arrays

--- brokenJaggedarray/PlayerData.cs
+++ brokenJaggedarray/PlayerData.cs
@@ -14,16 +14,18 @@ public class PlayerData //not deriving from Monobehaviour
     //automatically invoked because our class has the same name as the method (PlayerData)
     public PlayerData () //why don't i need a "void" here
     {
-        GameObject[] lines = ReturnLineinfo();
-
-
+        GameObject[] lines = ReturnLineinfo(); //so this line works fine.
+        arrayofLinesV2 = new Vector2[lines.Length][]; //that's how you initialise the number of vector2 arrays in our array of vector2 arrays.
+        Debug.Log($"arrayofLinesV2.length = {arrayofLinesV2.Length}");
         for (int i = 0; i < lines.Length; i++)
         {
-            arrayofLinesV2[i] = lines[i].GetComponent<EdgeCollider2D>().points; //will this work //apparently not
+            //Debug.Log($"{lines[i].GetComponent<EdgeCollider2D>().points.Length}"); so that works which means i can't fill it? oHH NEW
+            //arrayofLinesV2[i] = lines[i].GetComponent<EdgeCollider2D>().points; //error: Object Reference not set to an instance of an object?
+
+            arrayofLinesV2[i] = new Vector2[lines[i].GetComponent<EdgeCollider2D>().points.Length]; //sweet this code now works!
+            Debug.Log($"arrayofLinesV2[{i}].length = {arrayofLinesV2[i].Length}");
+
         }
-
-        
-
     }
     public GameObject[] ReturnLineinfo()
     {

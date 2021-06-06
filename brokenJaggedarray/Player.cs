eaa7c6f6bb1518cbddf27d5807e574930c624deb
willzstudios <83521347+willzstudios@users.noreply.github.com>
2021-06-06 17:36:39 +1000

    Add files via upload
    changed a bunch of Vector2[ ] into Float[ ] and then back again lmao
    - just to make the BinaryFormatter work

--- brokenJaggedarray/Player.cs
+++ brokenJaggedarray/Player.cs
@@ -13,31 +13,64 @@ public class Player : MonoBehaviour
     }
     public void LoadPlayer()
     {
+        //also need to reset scene here i think.
+
         //load the data
         PlayerData data = SaveSystem.LoadPlayer();
+        Debug.Log("DATA LOADED!");
         //then do stuff with the data
 
+        //ok how do we use
+        //data.XYcoordinates and data.sizeoflines
 
-        Vector2[][] arrayofLinesV2 = data.arrayofLinesV2;
-        for (int i = 0; i < arrayofLinesV2.Length; i++)
+        //just by using the XYcoordinates float[] and sizeoflines int[]
+
+        //ok it works for 1 line now but how do we make it load for X lines? more than 1?
+        //it seems like lines are separate but overlapping each other right now.
+        int count = 0;
+        for (int i = 0; i < data.sizeoflines.Length; i++) //for each line //once i goes to 1 what do we do
         {
             GameObject currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
             LineRenderer lineRenderer = currentLine.GetComponent<LineRenderer>();
             EdgeCollider2D edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
-            lineRenderer.SetPositions(V2ArraytoV3(arrayofLinesV2[i])); //why can't i convert V2array to V3 array implicitly
-            edgeCollider.points = arrayofLinesV2[i];
-        }
-       
-    }
 
-    public Vector3[] V2ArraytoV3(Vector2[] V2arrayInput)
+            float[] tempFloatarray = new float[data.sizeoflines[i]*2]; //new float array will have twice the amount of elements from sizeoflines[i], 2 floats per Vector2 element
+
+            int lastcount = count;
+            for (int x = 0 + lastcount; x < data.sizeoflines[i]*2 + lastcount; x++) 
+            {
+                tempFloatarray[x-lastcount] = data.XYcoordinates[x];
+                count += 1;
+            }
+
+            Vector2[] tempV2array = floatArraytoV2array(tempFloatarray); 
+            Vector3[] tempv3array = V2ArraytoV3(tempV2array);
+            lineRenderer.positionCount = tempv3array.Length; //this line is important haha.
+            lineRenderer.SetPositions(tempv3array); 
+            edgeCollider.points = tempV2array; //this works
+
+            //foreach (var x in tempv3array) Debug.Log(x.ToString());
+        }     
+    }
+    public Vector2[] floatArraytoV2array(float[] floatarrayinput) //function that changes a given float[] to a Vector2[] /this is working
     {
+        Vector2[] output = new Vector2[floatarrayinput.Length/2];
+        
+        for (int i = 0; i < floatarrayinput.Length; i+=2) //then length has to multiplied if you're incrementing by 2.
+        {
+            output[i/2] = new Vector2(floatarrayinput[i], floatarrayinput[i+1]); //error here//index went out of bounds of array
+        }
+
+        return output;
+    }
+    public Vector3[] V2ArraytoV3(Vector2[] V2arrayInput) //function that changes a given Vector2[] to a Vector3[]
+    {
+        Vector3[] outputV3 = new Vector3[V2arrayInput.Length];
+        
         for (int i = 0; i < V2arrayInput.Length; i++)
         {
-            outputV3[i] = new Vector3(V2arrayInput[i].x, V2arrayInput[i].y);
+            outputV3[i] = new Vector3 (V2arrayInput[i].x, V2arrayInput[i].y,0);
         }
         return outputV3;
     }
-
-
 }
--- brokenJaggedarray/PlayerData.cs
+++ brokenJaggedarray/PlayerData.cs
@@ -8,14 +8,20 @@ public class PlayerData //not deriving from Monobehaviour
 {//I think you can't change Vector2[][] tp binary lol not sure.
 
     //how do i create an array of Vector2 arrays
-    public Vector2[][] arrayofLinesV2; //you have to initialise it somehow to show number of arrays?
+    public float[] XYcoordinates;
+    public int[] sizeoflines; //size of this array = number of lines, values in this array = size of each line
     
     //this is a constructor class and will automatically be invoked when an instance of this class is created
     //automatically invoked because our class has the same name as the method (PlayerData)
     public PlayerData () //why don't i need a "void" here
     {
         GameObject[] lines = ReturnLineinfo(); //so this line works fine.
-        arrayofLinesV2 = new Vector2[lines.Length][]; //that's how you initialise the number of vector2 arrays in our array of vector2 arrays.
+        sizeoflines = new int[lines.Length];
+
+        Vector2[][] arrayofLinesV2 = new Vector2[lines.Length][]; //that's how you initialise the number of vector2 arrays in our array of vector2 arrays.
+
+        int coordinatesize = 0;
+
         Debug.Log($"arrayofLinesV2.length = {arrayofLinesV2.Length}");
         for (int i = 0; i < lines.Length; i++)
         {
@@ -24,8 +30,30 @@ public class PlayerData //not deriving from Monobehaviour
 
             arrayofLinesV2[i] = new Vector2[lines[i].GetComponent<EdgeCollider2D>().points.Length]; //sweet this code now works!
             Debug.Log($"arrayofLinesV2[{i}].length = {arrayofLinesV2[i].Length}");
+            sizeoflines[i] = arrayofLinesV2[i].Length;
+            
+            arrayofLinesV2[i] = lines[i].GetComponent<EdgeCollider2D>().points;
 
+            coordinatesize += arrayofLinesV2[i].Length;
         }
+        Debug.Log($"coordinatesize = {coordinatesize}");
+        int count = 0;
+        XYcoordinates = new float[coordinatesize*2]; //*2 because one Vector2 element = 2 float elements.
+        for (int i = 0; i < sizeoflines.Length; i++) //for each line
+        {
+            //each line
+            for (int x = 0; x < sizeoflines[i]*2; x += 2) //for each coordinate within the line //outside the bounds of teh array?
+            {
+                XYcoordinates[count] = arrayofLinesV2[i][x/2].x;
+                Debug.Log($"XYcoordinates[{count}] = {arrayofLinesV2[i][x / 2]}");
+                count += 1;
+                XYcoordinates[count] = arrayofLinesV2[i][x/2].y;   //if you increment by 2 you have to divide by two as well dumbass
+                //Debug.Log($"XYcoordinates[{count}] = {arrayofLinesV2[i][x / 2]}");
+                count += 1;
+            }
+        }
+        //foreach (var x in XYcoordinates) Debug.Log(x.ToString()); //ok from this we figured out the float array is working as intended.
+
     }
     public GameObject[] ReturnLineinfo()
     {
